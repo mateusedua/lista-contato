e855d0854c8f568632e2e6f953cd3d2992613bc2
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-04-15 20:58:59 -0300

    resolving conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-=======
->>>>>>> 4ec1ff30ba0bd1a38d249e4d5205bb44435ee7eb
 
 const nextConfig = {};
 
 export default nextConfig;
-<<<<<<< HEAD
 
 
-if (process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     await setupDevPlatform()
 }
-=======
->>>>>>> 4ec1ff30ba0bd1a38d249e4d5205bb44435ee7eb
