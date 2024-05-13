088b1032aafcc8854335cbb5a3b51c80fec5b21e
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-05-13 20:00:21 -0300

    feat: add select one contact

--- src/app/(private)/Contato/[[...slug]]/_actions/select-contato.tsx
+++ src/app/(private)/Contato/[[...slug]]/_actions/select-contato.tsx
@@ -2,7 +2,13 @@
 
 
 const selectContato = async (idcontato: string) => {
+    const result = await fetch(`${process.env.API_URL}api/contato/one/${idcontato}`)
+    const contato = await result.json()
 
+    return {
+        status: result.status,
+        data: contato[0]
+    }
 }
 
 export default selectContato
