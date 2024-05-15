896ce8f827882508d1250e55d5a03e79f48ee729
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-05-14 22:03:54 -0300

    implement function alter contact and add confirmation alert dialog for alter contact

--- src/app/(private)/Contato/[[...slug]]/page.tsx
+++ src/app/(private)/Contato/[[...slug]]/page.tsx
@@ -9,7 +9,6 @@ import {
     FormMessage
 } from "@/app/_components/ui/form"
 import Header from "@/app/_components/Header"
-import Image from "next/image"
 import { useForm } from "react-hook-form"
 import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -18,26 +17,34 @@ import { SelectValue } from "@/app/_components/ui/select"
 import formatPhone from "@/app/_lib/format-phone"
 import selectCategoria from "./_actions/select-categoria"
 import selectContato from "./_actions/select-contato"
+import alterarContato from "./_actions/alterar-contato"
 import { useEffect, useState } from "react"
 import { CategoriaProps } from "@/app/_helpers/interfaces"
+import { useRouter } from "next/navigation"
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/app/_components/ui/alert-dialog"
+import { Toaster, toast } from "sonner"
+import Link from "next/link"
 
 const formSchema = z.object({
     nome: z.string().trim().min(1, "Campo obrigatorio"),
     email: z.string().trim().min(1, "Campo obrigatorio").email("E-mail inválido"),
-    telefone: z.string().min(1, "Campo obrigatorio"),
+    celular: z.string().min(1, "Campo obrigatorio"),
     categoria: z.string().min(1, "Campo obrigatorio")
 })
 
 const Contato = ({ params }: any) => {
 
+    const router = useRouter()
+
     const [categoria, setCategoria] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             nome: "",
             email: "",
-            telefone: "",
+            celular: "",
             categoria: ""
         }
     })
@@ -46,8 +53,25 @@ const Contato = ({ params }: any) => {
         console.log(data)
     }
 
-    const handleSubmitAlterar = (data: z.infer<typeof formSchema>) => {
-        console.log(data)
+    const handleSubmitAlterar = async (data: z.infer<typeof formSchema>) => {
+
+        try {
+            setIsLoading(true)
+            const result = await alterarContato(data, params.slug[0])
+            if (result.status === 200 && result.data === true) {
+                toast.success('Contato alterado!', {
+                    duration: 1000,
+                    onAutoClose: () => router.push('/')
+                })
+            }
+            if (result.status !== 200) {
+                toast.error("Algo deu errado ao tentar alterar o contato! Tente novamente!")
+            }
+        } catch (err) {
+
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     useEffect(() => {
@@ -61,9 +85,11 @@ const Contato = ({ params }: any) => {
                     form.reset({
                         nome: oneContato.data.nome,
                         email: oneContato.data.email,
-                        telefone: oneContato.data.celular,
+                        celular: oneContato.data.celular,
                         categoria: oneContato.data.id_categoria
                     })
+                } else {
+                    router.push('/')
                 }
             }
         }
@@ -83,7 +109,9 @@ const Contato = ({ params }: any) => {
                                 <h1 className="font-bold text-3xl">Alterar Contato</h1>
                         }
                     </div>
-
+                    <div className="px-5 mb-3">
+                        <Link href='/'>Voltar</Link>
+                    </div>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(params.slug === undefined ? handleSubmitCadastrar : handleSubmitAlterar)} className="px-5 flex flex-col gap-4">
                             <FormField
@@ -112,11 +140,11 @@ const Contato = ({ params }: any) => {
                             />
                             <FormField
                                 control={form.control}
-                                name="telefone"
+                                name="celular"
                                 render={({ field }) => (
                                     <FormItem>
                                         <FormControl>
-                                            <Input placeholder="Telefone"
+                                            <Input placeholder="Celular"
                                                 value={field.value}
                                                 onChange={(e) => field.onChange(e.target.value = formatPhone(e.target.value))}
                                                 className="h-[50px]" />
@@ -148,18 +176,33 @@ const Contato = ({ params }: any) => {
                                     </FormItem>
                                 )}
                             />
-                            <Button type="submit" className="text-md h-[50px]">
-                                {
-                                    params.slug === undefined ?
-                                        <p>Cadastrar</p>
-                                        :
-                                        <p>Alterar</p>
-                                }
-                            </Button>
+                            {
+                                params.slug === undefined ?
+                                    <Button className="text-md h-[50px]" type='submit' disabled={isLoading}>Cadastrar</Button>
+                                    :
+                                    <AlertDialog>
+                                        <AlertDialogTrigger asChild disabled={isLoading}>
+                                            <Button className="text-md h-[50px]">Alterar</Button>
+                                        </AlertDialogTrigger>
+                                        <AlertDialogContent className="w-[90%]">
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle>Deseja alterar realmente o contato?</AlertDialogTitle>
+                                                <AlertDialogDescription>
+                                                    Essa ação não terá volta!
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                            <AlertDialogFooter>
+                                                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                                                <AlertDialogAction onClick={() => form.handleSubmit(handleSubmitAlterar)()} disabled={isLoading}>Confirmar</AlertDialogAction>
+                                            </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+                            }
                         </form>
                     </Form>
                 </div>
             </div>
+            <Toaster richColors />
         </div>
     )
 }
