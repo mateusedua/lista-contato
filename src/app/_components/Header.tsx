475d809dd9bd4a833718fc734574351ccbf72714
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-05-21 20:13:50 -0300

    feat: add logout

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,8 +1,7 @@
 import { Card, CardContent } from "./ui/card"
-import { Button } from "./ui/button"
 import Image from "next/image"
-import { MenuIcon } from 'lucide-react'
 import Link from "next/link"
+import DropDoewnMenuAcount from "./DropdownMenuAcount"
 
 export default function Header() {
     return (
@@ -11,9 +10,7 @@ export default function Header() {
                 <Link href='/'>
                     <Image src="/Logo.png" height={200} width={200} alt="Logo" />
                 </Link>
-                <Button variant="outline" size="icon">
-                    <MenuIcon size={26} />
-                </Button>
+                <DropDoewnMenuAcount />
             </CardContent>
         </Card>
     )
