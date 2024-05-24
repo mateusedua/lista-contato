dae3fa9a9b5048234f7f4df43cf6839250c7819a
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-05-23 21:11:05 -0300

    fix: test env file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
+    env: {
+        API_URL: 'https://api-lista-contato.mateuseduardoteuta10.workers.dev/'
+    }
 };
 
 export default nextConfig;
